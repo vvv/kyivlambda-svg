73c3a93bb1a3725933dd26a070c0c4bb4d36e49e
Valery V. Vorotyntsev <valery.vv@gmail.com>
2019-05-05 12:57:41 +0300

    [WIP] Start rewriting with Elm Architecture in mind

--- src/Main.elm
+++ src/Main.elm
@@ -1,16 +1,327 @@
 module Main exposing (main)
 
+import Browser
 import Html exposing (Html)
-import Svg
+import Svg exposing (Svg)
 import Svg.Attributes as SvgA
 
 
-main : Html msg
+main : Program () Model msg
 main =
-    Html.div []
-        [ logoHaskell
-        , logoKyivMetro
+    Browser.sandbox
+        { init = init
+        , view = view
+        , update = \_ model -> model
+        }
+
+
+----------------------------------------------------------------------
+-- MODEL
+
+
+type Model
+    = Elm
+    | Haskell
+    | KyivMetro
+    | KyivHaskell
+    | KyivElm
+    | KyivRust
+    | KyivLambda
+
+
+init : Model
+init =
+    KyivMetro
+
+
+----------------------------------------------------------------------
+-- VIEW
+
+
+view : Model -> Html msg
+view model =
+    Svg.svg (attributes model) (svgs model)
+
+
+----------------------------------------------------------------------
+
+
+attributes : Model -> List (Svg.Attribute msg)
+attributes model =
+    case model of
+        Haskell ->
+            [ SvgA.viewBox "0 0 170 120" ]
+
+        KyivMetro ->
+            [ SvgA.width "311.75", SvgA.height "363.125" ]
+
+        _ ->
+            Debug.todo "XXX IMPLEMENTME"
+
+
+svgs : Model -> List (Svg msg)
+svgs model =
+    case model of
+        Haskell ->
+            Debug.todo "XXX IMPLEMENTME"
+
+        KyivMetro ->
+            List.map (path model)
+                [ outerArc
+                , innerSemicircle
+                , letterM
+                , leavesBg
+                , leafLeft
+                , leafMiddle
+                , leafRight
+                ]
+
+        _ ->
+            Debug.todo "XXX IMPLEMENTME"
+
+
+----------------------------------------------------------------------
+-- Helpers
+
+
+type alias Path =
+    { d : String
+    , style : Model -> PathStyle
+    }
+
+
+path : Model -> Path -> Svg msg
+path model p =
+    Svg.path
+        [ SvgA.d p.d
+        , SvgA.style <| fromPathStyle <| p.style model
         ]
+        []
+
+
+-- XXX Use `typed-svg` package.
+
+
+type StrokeLinejoin
+    = Bevel
+    | Miter
+
+
+fromStrokeLinejoin : StrokeLinejoin -> String
+fromStrokeLinejoin x =
+    case x of
+        Bevel ->
+            "bevel"
+
+        Miter ->
+            "miter"
+
+
+type alias PathStyle =
+    { fill : String
+    , strokeWidth : Float
+    , strokeLinejoin : StrokeLinejoin
+    }
+
+
+fromPathStyle : PathStyle -> String
+fromPathStyle ps =
+    "fill:"
+        ++ ps.fill
+        ++ "; stroke-width:"
+        ++ String.fromFloat ps.strokeWidth
+        ++ "; stroke-linejoin:"
+        ++ fromStrokeLinejoin ps.strokeLinejoin
+
+
+----------------------------------------------------------------------
+-- Haskell logo adapted from
+-- https://upload.wikimedia.org/wikipedia/commons/1/1c/Haskell-Logo.svg
+
+
+element1_XXX : Path
+element1_XXX =
+    Path
+        """
+        M 0,120
+        L 40,60
+        L 0,0
+        L 30,0
+        L 70,60
+        L 30,120
+        z
+        """
+        (Debug.todo "XXX IMPLEMENTME")
+
+
+----------------------------------------------------------------------
+-- Kyiv Metro logo adapted from
+-- https://upload.wikimedia.org/wikipedia/commons/b/be/Kyiv_Metro_logo.svg
+
+
+colorLeaf : Model -> String
+colorLeaf model =
+    if model == KyivMetro then
+        "#00923f"
+
+    else
+        "#463c62"
+
+
+colorEdge : Model -> String
+colorEdge model =
+    if model == KyivMetro then
+        "#fef406"
+
+    else
+        "lightgrey"
+
+
+style1_XXX : Model -> PathStyle
+style1_XXX model =
+    PathStyle (colorEdge model) 0.5 Bevel
+
+
+style2_XXX : Model -> PathStyle
+style2_XXX model =
+    PathStyle (colorLeaf model) 0.1 Miter
+
+
+style3_XXX : Model -> PathStyle
+style3_XXX model =
+    PathStyle (colorEdge model) 3.5 Miter
+
+
+style4_XXX : Model -> PathStyle
+style4_XXX model =
+    PathStyle (colorLeaf model) 1 Miter
+
+
+outerArc : Path
+outerArc =
+    Path
+        """
+        M 155.875,0
+        C 77.491,0 13.875,64.512 13.875,144
+        C 13.875,162.34831 17.321494,179.85689 23.5,196
+        L 32,196
+        C 25.447068,179.98274 21.875,162.41298 21.875,144
+        C 21.875,68.9902 81.907,8.125 155.875,8.125
+        C 229.843,8.125 289.875,68.9902 289.875,144
+        C 289.875,162.41298 286.30294,179.98274 279.75,196
+        L 288.25,196
+        C 294.4285,179.85689 297.875,162.34831 297.875,144
+        C 297.875,64.512 234.259,0 155.875,0
+        z
+        """
+        style1_XXX
+
+
+innerSemicircle : Path
+innerSemicircle =
+    Path
+        """
+        M 155.875,16.25
+        C 86.323,16.25 29.875,73.46839 29.875,144
+        C 29.875,162.51455 33.75476,180.11183 40.75,196
+        L 271,196
+        C 277.99524,180.11183 281.875,162.51455 281.875,144
+        C 281.875,73.4684 225.427,16.24999 155.875,16.25
+        z
+        """
+        style1_XXX
+
+
+letterM : Path
+letterM =
+    Path
+        """
+        M 110.875,44
+        L 44.875,178
+        L 89.875,178
+        L 114.875,128
+        L 155.875,178
+        L 196.875,128
+        L 221.875,178
+        L 266.875,178
+        L 200.875,44
+        L 155.875,106
+        L 110.875,44
+        z
+        """
+        style2_XXX
+
+
+leavesBg : Path
+leavesBg =
+    Path
+        """
+        M 35,206
+        L 0,304
+        L 85.25,304
+        L 140.125,236.25
+        L 100.125,305
+        L 155.875,363.125
+        L 211.625,305
+        L 171.75,236.25
+        L 226.5,304
+        L 311.75,304
+        L 276.75,206
+        L 164.5,206
+        L 147.25,206
+        L 35,206
+        z
+        """
+        style3_XXX
+
+
+leafLeft : Path
+leafLeft =
+    Path
+        """
+        M 9.875,297
+        L 39.875,213
+        L 149.875,213
+        L 81.875,297
+        L 9.875,297
+        z
+        """
+        style4_XXX
+
+
+leafMiddle : Path
+leafMiddle =
+    Path
+        """
+        M 155.875,353
+        L 108.875,304
+        L 155.875,223
+        L 202.875,304
+        L 155.875,353
+        z
+        """
+        style2_XXX
+
+
+leafRight : Path
+leafRight =
+    Path
+        """
+        M 301.875,297
+        L 271.875,213
+        L 161.875,213
+        L 229.875,297
+        L 301.875,297
+        z
+        """
+        style2_XXX
+
+
+{--XXX
+colorLeaf : String
+colorLeaf =
+    -- "#00923f"
+    "#463c62"
 
 
 -- Adapted from
@@ -20,10 +331,7 @@ main =
 logoHaskell : Html msg
 logoHaskell =
     Svg.svg
-        [ SvgA.width "340"
-        , SvgA.height "240"
-        , SvgA.viewBox "0 0 170 120"
-        ]
+        [ SvgA.viewBox "0 0 170 120" ]
         [ Svg.path
             [ SvgA.d
                 """
@@ -35,7 +343,7 @@ logoHaskell =
                 L 30,120
                 z
                 """
-            , SvgA.style "fill:#463c62"
+            , SvgA.style <| "fill:" ++ colorLeaf
             ]
             []
         , Svg.path
@@ -88,10 +396,8 @@ logoKyivMetro : Html msg
 logoKyivMetro =
     let
         yellow =
-            "#fef406"
-
-        green =
-            "#00923f"
+            "lightgrey"
+            -- "#fef406"
 
         style1 =
             SvgA.style <|
@@ -102,7 +408,7 @@ logoKyivMetro =
         style2 =
             SvgA.style <|
                 "fill:"
-                    ++ green
+                    ++ colorLeaf
                     ++ "; stroke-width:0.1; stroke-linejoin:miter"
 
         style3 =
@@ -114,7 +420,7 @@ logoKyivMetro =
         style4 =
             SvgA.style <|
                 "fill:"
-                    ++ green
+                    ++ colorLeaf
                     ++ "; stroke-width:1; stroke-linejoin:miter"
 
         outerArc =
@@ -249,10 +555,14 @@ logoKyivMetro =
         , SvgA.height "363.125"
         ]
         [ outerArc
-        , innerSemicircle
-        , letterM
-        , leavesBg
+        -- , innerSemicircle
+        -- , letterM
+        -- , leavesBg
         , leafLeft
         , leafMiddle
         , leafRight
+        , Svg.g
+            [ SvgA.transform "scale(0.65) translate(105 7)" ]
+            [ logoHaskell ]
         ]
+--}
